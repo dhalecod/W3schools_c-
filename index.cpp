4e23123e2dd609bbc78cb9dd1c7b36b5dfd2577b
dhalecod <williammakinde408@gmail.com>
2024-11-29 20:45:31 +0000

    updated

--- index.cpp
+++ index.cpp
@@ -1,16 +1,8 @@
 #include <iostream>
-using namespace std;
 int main() {
-    string name = "Makinde Williams";
-    int idNum = 3110;
-    int age = 21;
-    char grade = 'A';
-    float score = 99.9;
-
-    cout << "Student Name: " << name << "\n";
-    cout << "Student ID: " << idNum << "\n";
-    cout << "Student Age: " << age << "\n";
-    cout << "Student Grade: " << grade << "\n";
-    cout << "Student Score: " << score << "\n";
+    int x;
+    cout << "input number";
+    cin >> x;
+    cout << "Your number is: " << x;
     return 0;
 }
