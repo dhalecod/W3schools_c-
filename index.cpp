3ebcfb74712a358b3ffe294b3c6d505694a0762b
dhalecod <williammakinde408@gmail.com>
2024-11-29 14:45:29 +0000

    updated

--- index.cpp
+++ index.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 int main() {
-    cout << "I am Dev_Marcus";
-    cout << "I am a software engineer";
+    cout << "I am Dev_Marcus\n";
+    cout << "I am a software engineer\n";
     cout << "I am a Nigerian";
     return 0;
 }
