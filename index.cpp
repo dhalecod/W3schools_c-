6c9cf32eb27a2ef777714b438e79abf5be22ad31
dhalecod <williammakinde408@gmail.com>
2024-11-29 15:52:24 +0000

    updated

--- index.cpp
+++ index.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 int main() {
     int myNum = 22;
-    cout << "I am Dev_Marcus, I am " myNum "Years old";
+    cout << "I am Dev_Marcus, I am " << myNum << "Years old";
     return 0;
 }
