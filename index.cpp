ddb3493d4488034b2c5af4f325310fdd4690b33d
dhalecod <williammakinde408@gmail.com>
2024-11-30 06:36:41 +0000

    updated

--- index.cpp
+++ index.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int x;
-    cout << "input number\n";
-    cin >> x;
-    cout << "Your number is: " << x;
+    int num1;
+    int num2;
+    int sum;
+    cout << "input first number \n";
+    cin >> num1;
+    cout << "Input second num \n";
+    cin >> num2;
+    sum = num1 + num2;
+    cout << "The sum of your numbers is: "sum;
     return 0;
 }
+   
